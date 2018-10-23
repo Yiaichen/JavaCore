ffd7c4d1053b2a86e4d4ffdf6ac1f2664e45b285
vayi <chu@ittx.com.cn>
2018-10-23 18:32:54 +0800

    💕 leetcode 字符串初级训练 💕

---
+++ javaDemo/leetcode/string/IsPalindrome.java
@@ -0,0 +1,42 @@
+package javaDemo.leetcode.string;
+
+/**
+ * 验证回文字符串
+ *
+ * 给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
+ *
+ * 说明：本题中，我们将空字符串定义为有效的回文串。
+ *
+ * 示例 1:
+ * 输入: "A man, a plan, a canal: Panama"
+ * 输出: true
+ *
+ * 示例 2:
+ * 输入: "race a car"
+ * 输出: false
+ */
+public class IsPalindrome {
+
+    public static void main(String[] args) {
+        String s = "race a car";
+        System.out.println(isPalindrome(s));
+    }
+
+    public static boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        StringBuilder l = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if ((s.charAt(i) - 0 >= 48 && s.charAt(i) - 0 <= 57) || (s.charAt(i) - 0 >= 97 && s.charAt(i) - 0 <= 122)) {
+                l.append(s.charAt(i));
+            }
+        }
+        int j = 0, k = l.length() - 1;
+        while (j < k) {
+            if (l.charAt(j++) != l.charAt(k--)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
---
+++ javaDemo/leetcode/string/ReverseNum.java
@@ -0,0 +1,24 @@
+package javaDemo.leetcode.string;
+
+public class ReverseNum {
+    public static void main(String[] args) {
+        int x = -12300;
+        System.out.println(reverseNum(x));
+    }
+
+    public static int reverseNum(Integer x) {
+        int result = 0;
+
+        while (x != 0) {
+            // 因为都是用int型的，如果超出了范围，其除以10的结果就不会跟之前的结果一致，通过这点也可以进行区分
+            int tmp = result * 10 + x % 10;
+            if (tmp / 10 != result) {
+                return 0;
+            }
+            result = tmp;
+            x = x / 10;
+        }
+
+        return result;
+    }
+}
